875d16dc1cd460da3b6d2d1c93f2adcca3d3c884
VVJS-22 <s.jayeshchennai@gmail.com>
2021-09-18 08:48:23 +0530

    first commit

---
+++ components/Menu/style.js
@@ -0,0 +1,36 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+    menuItems: {
+        marginTop: 20,
+        marginLeft: 20,
+        marginBottom: 20
+    },
+    icon: {
+        color: "#fff"
+    },
+    menuText: {
+        color: "#fff",
+        fontSize: 16,
+        fontWeight: "bold",
+        marginLeft: 15,
+        textTransform: "uppercase"
+    },
+    menuRow: {
+        marginTop: 15,
+        marginBottom: 15,
+        flexDirection: "row",
+        alignItems: 'center'
+    },
+    menuTextBox: {
+        flexGrow: 1
+    },
+    arrowIcon: {
+        color: '#4d4d4e'
+    },
+    subTitle: {
+        color: 'grey',
+        fontSize: 14,
+        marginLeft: 15
+    }
+})
